ffd9d829e9eabe7cd765bea8fb342fa40be70d03
Vali Tuguran <constantin.tuguran@ymens.com>
2017-07-26 11:33:04 +0300

    Added Requirements doc for the bookstore app and project initial version.

---
+++ bookstore/src/com/ymens/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.ymens.helloworld;
+
+/**
+ * Created by Constantin.Tuguran on 7/26/2017.
+ */
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
