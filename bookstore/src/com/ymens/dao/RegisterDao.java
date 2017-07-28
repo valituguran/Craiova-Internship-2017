3b6a7aff7906474a8911886715a692e7a56d2991
madalina.luca <madalina.luca12@yahoo.com>
2017-07-28 10:38:18 +0300

    Added login and register servlets.

---
+++ bookstore/src/com/ymens/dao/RegisterDao.java
@@ -0,0 +1,68 @@
+package com.ymens.dao;
+
+import java.sql.*;
+/**
+ * Created by madalina.luca on 7/27/2017.
+ */
+public class RegisterDao {
+    public static boolean validate(String name, String pass, String email) {
+        boolean status = false;
+        Connection conn = null;
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+
+        String url = "jdbc:mysql://localhost:3306/";
+        String dbName = "login";
+        String driver = "com.mysql.jdbc.Driver";
+        String userName = "root";
+        String password = "root";
+        PreparedStatement ps = null;
+
+        try {
+            Class.forName(driver).newInstance();
+            conn = DriverManager
+                    .getConnection(url + dbName, userName, password);
+
+            ps = conn.prepareStatement
+                    ("insert into users values(?,?,?)");
+
+            ps.setString(1, name);
+            ps.setString(2, email);
+            ps.setString(3, pass);
+            if(name.isEmpty()||pass.isEmpty()||email.isEmpty())
+            {
+                status = true;
+            }
+            else
+            {
+                status = false;
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return status;
+    }
+}
---
+++ bookstore/src/com/ymens/servlet/RegisterServlet.java
@@ -0,0 +1,45 @@
+package com.ymens.servlet;
+
+
+import com.ymens.dao.RegisterDao;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by madalina.luca on 7/27/2017.
+ */
+public class RegisterServlet {
+    private static final long serialVersionUID = 1L;
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        String n=request.getParameter("username");
+        String p=request.getParameter("userpass");
+        String e=request.getParameter("email");
+        HttpSession session = request.getSession(false);
+        if(session!=null)
+            session.setAttribute("name", n);
+
+        if(RegisterDao.validate(n, p, e) == true ){
+            RequestDispatcher rd=request.getRequestDispatcher("register.jsp");
+            rd.forward(request,response);
+        }
+        else{
+            RequestDispatcher rd = request.getRequestDispatcher("register.jsp");
+            out.println("<font color=red>Please fill all the fields</font>");
+            rd.include(request, response);
+        }
+
+        out.close();
+    }
+}
