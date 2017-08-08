70a614da8631da1094c7c879448dbe3a29ca3a76
madalina.luca <madalina.luca12@yahoo.com>
2017-08-08 10:55:26 +0300

    Added database sql.

--- bookstore/dbexport/Export2017-08-08-10-50.sql
+++ bookstore/dbexport/Export2017-08-08-10-50.sql
@@ -12,12 +12,22 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 
-/* EXPORT 8 AUGUST 10:44*/
-
 -- Dumping database structure for bookstore
 DROP DATABASE IF EXISTS `bookstore`;
 CREATE DATABASE IF NOT EXISTS `bookstore` /*!40100 DEFAULT CHARACTER SET latin1 */;
 USE `bookstore`;
+
+-- Dumping structure for table bookstore.authors
+DROP TABLE IF EXISTS `authors`;
+CREATE TABLE IF NOT EXISTS `authors` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `age` varchar(50) DEFAULT NULL,
+  `nationality` varchar(50) DEFAULT NULL,
+  `description` longtext,
+  `CNP` bigint(13) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table bookstore.authors: ~6 rows (approximately)
 DELETE FROM `authors`;
@@ -31,6 +41,22 @@ INSERT INTO `authors` (`id`, `name`, `age`, `nationality`, `description`, `CNP`)
 	(36, 'nume', '45', 'nat', 'descr', 0);
 /*!40000 ALTER TABLE `authors` ENABLE KEYS */;
 
+-- Dumping structure for table bookstore.books
+DROP TABLE IF EXISTS `books`;
+CREATE TABLE IF NOT EXISTS `books` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `author_id` int(11) NOT NULL,
+  `isbn` int(30) NOT NULL,
+  `price` double NOT NULL,
+  `description` longtext NOT NULL,
+  `image` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `isbn` (`isbn`),
+  KEY `FK_books_authors` (`author_id`),
+  CONSTRAINT `FK_books_authors` FOREIGN KEY (`author_id`) REFERENCES `authors` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
+
 -- Dumping data for table bookstore.books: ~6 rows (approximately)
 DELETE FROM `books`;
 /*!40000 ALTER TABLE `books` DISABLE KEYS */;
@@ -43,15 +69,52 @@ INSERT INTO `books` (`id`, `name`, `author_id`, `isbn`, `price`, `description`, 
 	(24, 'nume', 35, 12647, 37, 'hafahj', '../images/images.jpg');
 /*!40000 ALTER TABLE `books` ENABLE KEYS */;
 
+-- Dumping structure for table bookstore.order
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE IF NOT EXISTS `order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `total_price` double DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- Dumping data for table bookstore.order: ~0 rows (approximately)
 DELETE FROM `order`;
 /*!40000 ALTER TABLE `order` DISABLE KEYS */;
 /*!40000 ALTER TABLE `order` ENABLE KEYS */;
 
+-- Dumping structure for table bookstore.order_item
+DROP TABLE IF EXISTS `order_item`;
+CREATE TABLE IF NOT EXISTS `order_item` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) DEFAULT NULL,
+  `book_id` int(11) DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_order_item_order` (`order_id`),
+  KEY `FK_order_item_books` (`book_id`),
+  CONSTRAINT `FK_order_item_books` FOREIGN KEY (`book_id`) REFERENCES `books` (`id`),
+  CONSTRAINT `FK_order_item_order` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 -- Dumping data for table bookstore.order_item: ~0 rows (approximately)
 DELETE FROM `order_item`;
 /*!40000 ALTER TABLE `order_item` DISABLE KEYS */;
 /*!40000 ALTER TABLE `order_item` ENABLE KEYS */;
+
+-- Dumping structure for table bookstore.users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `real_name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `type` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  KEY `FK_users_user_type` (`type`),
+  CONSTRAINT `FK_users_user_type` FOREIGN KEY (`type`) REFERENCES `user_type` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table bookstore.users: ~10 rows (approximately)
 DELETE FROM `users`;
@@ -68,6 +131,14 @@ INSERT INTO `users` (`id`, `username`, `password`, `real_name`, `email`, `type`)
 	(24, 'admin', 'admin', 'Madalina Luca', 'madalina.luca12@yahoo.com', 1),
 	(25, 'user', 'user', 'Maria Popa', 'maria.popa@yahoo.com', 2);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+-- Dumping structure for table bookstore.user_type
+DROP TABLE IF EXISTS `user_type`;
+CREATE TABLE IF NOT EXISTS `user_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 -- Dumping data for table bookstore.user_type: ~2 rows (approximately)
 DELETE FROM `user_type`;
