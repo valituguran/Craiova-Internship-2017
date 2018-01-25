51634acf347296537e286b1e57838d5e7c25c65b
lucamadalina <madalina.luca12@yaoo.com>
2018-01-25 10:44:57 +0200

    Added user classes

--- TwitterClone/src/main/java/com/twitter/entity/User.java
+++ TwitterClone/src/main/java/com/twitter/entity/User.java
@@ -1,4 +1,53 @@
 package com.twitter.entity;
 
 public class User {
+
+    private int id;
+    private String firstname;
+    private String lastname;
+    private String username;
+    private String password;
+    private String email;
+    private String adress;
+
+    public User(int id, String firstname, String lastname, String username, String password, String email, String adress) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.adress = adress;
+    }
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
 }
