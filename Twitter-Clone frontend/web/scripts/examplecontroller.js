337d7e6ebf764009a17dc32c79e003f6847797d8
NIonutLucian <nicolescuionutluci@gmail.com>
2018-02-06 10:46:40 +0200

    js

---
+++ Twitter-Clone frontend/web/scripts/examplecontroller.js
@@ -0,0 +1,12 @@
+var app =  angular.module('twitterclone', []);
+app.controller('example', function($scope, $http) {
+    console.log("a intat in get");
+    $http.get('http://localhost:8080/5a7823e99b18960f18f4d409',{headers:{'Content-type':'application/json'}}).
+    then(function(response) {
+        $scope.r = response.data;
+        $scope.exemplu = "exemplu";
+        console.log("a intat in get");
+        console.log($scope.rezultat + "dfgdsds");
+        console.log(response.data + "fgdssdf");
+    });
+});
